621381b99a1d9db2cde8bd42785648c2a0242b03
Jordan Hambright <jhambright62@gmail.com>
2017-03-28 14:25:30 +0000

    i've improved game

--- scripts.js
+++ scripts.js
@@ -2,19 +2,18 @@
     //Once your code is hooked up you should see this message on your web page: “Scripts are linked”
     $("body").append('Scripts are linked.');
     //Put all of your code under here.
+     var box = Math.floor(Math.random() * 20);
     $('button').click(function(){
     console.log('You clicked?');
         var guess = $('input').val()
-        $('body').append(guess)
-         var box = 3;
-    if (box == guess){
-        $('body').append("That's it!");
-    } else if (box<guess){
-         $('body').append("Its Too High");
-    }
-    
-    else {
         
+        
+    if (box == guess){
+        $('.response').html("That's it!");
+    } else if (box<guess){
+         $('.response').html("Its Too High");
+    } else {
+        $('.response').html("Its Too Low");
     }
     });
    
